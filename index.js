a705ab35a6fbfe784d0609205f2cd301a2e84ba1
jiftoo <dan.kov@jiftoo.dev>
2022-09-25 13:40:45 +0300

    randomly select 2 or 3 series and fix stack overflow err

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function genRandomSeries(steps, mag) {
 	};
 }
 
-function renderSeries(series, i) {
+function renderSeries(series, i, add) {
 	step = 400;
 	let start = [0, Math.min(Math.max(series(10) + 400, 300), 350)];
 
@@ -37,8 +37,8 @@ function renderSeries(series, i) {
 	let lastY = start[1];
 	for (let i = 1; i < canvas.width / step + 10; i++) {
 		let x = i * step;
-		let y = start[1] + series(x);
-		let y2 = start[1] + series(x);
+		let y = start[1] + series(x + add);
+		let y2 = start[1] + series(x + add);
 		ctx.bezierCurveTo(lastX / 2 + x / 2, lastY, lastX / 2 + x / 2, y, x, y);
 		lastX = x;
 		lastY = y;
@@ -51,10 +51,10 @@ gradient.addColorStop(0, "rgba(255, 166, 0, 0.25)");
 gradient.addColorStop(0.65, "rgba(255, 0, 255, 0.25)");
 gradient.addColorStop(1, "rgba(0, 128, 255, 0.5)");
 
-let seriesCount = 2;
+let seriesCount = Math.random() < 0.5 ? 2 : 3;
 let seriesArray = Array(seriesCount)
 	.fill(0)
-	.map(() => genRandomSeries(115, 8));
+	.map(() => [0, genRandomSeries(115, 8)]);
 let shift = -110;
 
 function animate(time) {
@@ -64,24 +64,23 @@ function animate(time) {
 	ctx.filter = "blur(1px)";
 	shift -= 0.01;
 
-	seriesArray.forEach((series, i) => {
+	seriesArray.forEach(([add, series], i) => {
 		ctx.translate(shift, 0);
-		renderSeries(series, i);
+		renderSeries(series, i, add);
 	});
 	ctx.translate(-shift * seriesCount, 0);
 
 	ctx.lineWidth = 10;
 	ctx.strokeStyle = "rgba(255,255,255,0.2)";
 	let trBack = 40;
-	seriesArray.forEach((series, i) => {
+	seriesArray.forEach(([add, series], i) => {
 		ctx.translate(shift, 0);
-		renderSeries(series, i);
+		renderSeries(series, i, add);
 	});
 	ctx.translate(-shift * seriesCount, 0);
 
-	seriesArray = seriesArray.map((series, i) => {
-		const parallax = (x) => x * (i / 1000000); //?
-		return (x) => series(x + parallax(x) + 0.0021);
+	seriesArray = seriesArray.map(([add, series], i) => {
+		return [add + i / 1000000 + 0.0021, series];
 	});
 
 	window.requestAnimationFrame(animate);
