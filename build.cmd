be758d506ae7790d6040d8840b6c98a83830e89a
Dan Kov <0x666c@mail.ru>
2022-09-25 03:08:23 +0300

    add minify script and move js to file

---
+++ build.cmd
@@ -0,0 +1,10 @@
+@echo off
+rm -r ./build
+mkdir build
+copy /Y favicon.ico build
+copy /Y *.svg build
+call pnpx html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype index.html -o build/index.html
+call pnpx clean-css-cli -O2 style.css > build/style.css
+call pnpx terser index.js -o build/index.js
+
+echo "Done"
