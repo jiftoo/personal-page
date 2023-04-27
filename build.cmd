0d99f37c45e6cdeeddffaaf8db2fc763c9bdf0ed
Dan Kov <0x666c@mail.ru>
2023-04-27 15:34:23 +0300

    remove brain damaged param from build

--- build.cmd
+++ build.cmd
@@ -3,7 +3,7 @@ rm -r ./build
 mkdir build
 copy /Y favicon.ico build
 copy /Y *.svg build
-call pnpx html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype index.html -o build/index.html
+call pnpx html-minifier --collapse-whitespace --remove-comments --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype index.html -o build/index.html
 call pnpx clean-css-cli -O2 style.css > build/style.css
 call pnpx terser index.js -o build/index.js
 
